b649eeeb78ed2b7b986409b3e6fb783568992f27
MalikAtalla <MalikAtalla@hotmail.com>
2014-09-24 08:21:54 -0700

    Project Setup

---
+++ checks/src/main/java/de/malikatalla/checkstyle/checks/EqualsHashcodeCompletenessCheck.java
@@ -0,0 +1,30 @@
+package de.malikatalla.checkstyle.checks;
+
+import com.puppycrawl.tools.checkstyle.api.Check;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+
+public class EqualsHashcodeCompletenessCheck extends Check {
+	private static final int DEFAULT_MAX = 30;
+	private int max = DEFAULT_MAX;
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF };
+	}
+
+	@Override
+	public void visitToken(DetailAST ast) {
+		// find the OBJBLOCK node below the CLASS_DEF/INTERFACE_DEF
+		DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
+		// count the number of direct children of the OBJBLOCK
+		// that are METHOD_DEFS
+		int methodDefs = objBlock.getChildCount(TokenTypes.METHOD_DEF);
+		// report error if limit is reached
+		if (methodDefs > this.max) {
+			log(ast.getLineNo(), "too many methods, only " + this.max
+					+ " are allowed");
+		}
+	}
+}
---
+++ checks/test/CorrectFile.java
@@ -0,0 +1,96 @@
+package gaia.domain.model;
+
+import com.google.common.base.Objects;
+
+public class Result extends SecureObject {
+
+    private static final String USER_FRIENDLY_NAME = "Result";
+
+    private double value;
+    private String unit;
+    private String sampleFraction;
+    private String statusIdentifier;
+    private String valueType;
+
+    public static String getUserFriendlyName() {
+        return USER_FRIENDLY_NAME;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getSampleFraction() {
+        return sampleFraction;
+    }
+
+    public void setSampleFraction(String sampleFraction) {
+        this.sampleFraction = sampleFraction;
+    }
+
+    public String getStatusIdentifier() {
+        return statusIdentifier;
+    }
+
+    public void setStatusIdentifier(String statusIdentifier) {
+        this.statusIdentifier = statusIdentifier;
+    }
+
+    public String getValueType() {
+        return valueType;
+    }
+
+    public void setValueType(String valueType) {
+        this.valueType = valueType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id, tenant, customId, value, unit, sampleFraction, statusIdentifier, valueType);
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                .add("id", id)
+                .add("tenant", tenant)
+                .add("customId", customId)
+                .add("value", value)
+                .add("unit", unit)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || !(obj instanceof Result)) {
+            return false;
+        }
+        return equals((Result) obj);
+    }
+
+    public boolean equals(final Result other) {
+        return Objects.equal(unit, other.unit) &&
+                Objects.equal(value, other.value) &&
+                Objects.equal(id, other.id) &&
+                Objects.equal(tenant, other.tenant) &&
+                Objects.equal(customId, other.customId) &&
+                Objects.equal(sampleFraction, other.sampleFraction) &&
+                Objects.equal(statusIdentifier, other.statusIdentifier) &&
+                Objects.equal(valueType, other.valueType);
+    }
+}
